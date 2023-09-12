2dd89c22b64138b1202fb8a840c49a92486cd90f
EnricoValente <enricovalente42@gmail.com>
2023-09-12 23:52:48 +0200

    commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,27 @@ Homepage
 
 
 @section('main-content')
-<h1>
-    Template repository
-</h1>
+<div class="container">
+    <h1>
+        Trains
+    </h1>
+
+    <div class="row">
+        @foreach ($trains as $train)
+        
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h3>
+                            {{$train->company}}
+                        </h3>
+                        <div>
+                            {{$train->departure_station}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
 @endsection
